e63e05d1b29fc5b4e685c208cd79439ed57dd219
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-26 02:12:46 +0600

    async await done

--- app.js
+++ app.js
@@ -1,2 +1,55 @@
 const stateControll = true;
-const mark = 'X';
+const mark = 100;
+
+function enroll() {
+    console.log('Enrollment in progress...');
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (stateControll) {
+                resolve();
+            } else {
+                reject('Payment failed');
+            }
+        }, 1000);
+    });
+    return promise;
+}
+function progress() {
+    console.log('Continue');
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (mark > 90) {
+                resolve();
+            } else {
+                reject("You don't have emough mark to get the cirtificate");
+            }
+        }, 1000);
+    });
+    return promise;
+}
+function certificate() {
+    console.log('Certificate in progress...');
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("You have got the certificate");
+        }, 1000);
+    });
+    return promise;
+}
+
+// enroll().then(progress).then(certificate).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// });
+async function asyncFunction() {
+    try {
+        await enroll();
+        await progress();
+        const result = await certificate();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+asyncFunction();
